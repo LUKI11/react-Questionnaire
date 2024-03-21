741a73ec0ec28868b6feb3ef02840aeb731059a3
luki <Luki.Qi@outlook.com>
2024-03-21 23:54:34 +1100

    Stat header middle part, Link and QRCode function and element

--- src/pages/question/Stat/StatHeader.tsx
+++ src/pages/question/Stat/StatHeader.tsx
@@ -1,15 +1,52 @@
-import React, { FC } from 'react';
-import { Row, Col, Button, Typography, Space } from 'antd';
+import React, { FC, useState } from 'react';
+import {
+  Row,
+  Col,
+  Button,
+  Typography,
+  Space,
+  Input,
+  Tooltip,
+  message,
+  QRCode,
+  Popover,
+} from 'antd';
 import styles from './StatHeader.module.scss';
-import { LeftOutlined } from '@ant-design/icons';
+import { LeftOutlined, CopyOutlined, QrcodeOutlined } from '@ant-design/icons';
 import useGetPageInfo from '../../../hooks/useGetPageInfo';
 import { useNavigate, useParams } from 'react-router';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const { Title } = Typography;
 const StatHeader: FC = () => {
   const { title } = useGetPageInfo();
   const navigate = useNavigate();
   const { id } = useParams();
+
+  // link and QRCode element, middle part
+  const genLinkAndQRCode = () => {
+    const url = `http://localhost:3000/question/${id}`;
+    const QRCodeElem = <QRCode value={url || '-'}></QRCode>;
+    return (
+      <Space>
+        <Input value={url} style={{ width: '250px' }}></Input>
+        <Tooltip title="Copy URL">
+          <CopyToClipboard
+            text={url}
+            onCopy={() => {
+              message.success('copy successfully');
+            }}
+          >
+            <Button icon={<CopyOutlined />}></Button>
+          </CopyToClipboard>
+        </Tooltip>
+        <Popover content={QRCodeElem} placement="bottom">
+          <Button icon={<QrcodeOutlined />}></Button>
+        </Popover>
+      </Space>
+    );
+  };
+
   return (
     <div className={styles['header-wrapper']}>
       <Row className={styles.header}>
@@ -22,7 +59,7 @@ const StatHeader: FC = () => {
           </Space>
         </Col>
         <Col className={styles.mid} span={16}>
-          Middle
+          {genLinkAndQRCode()}
         </Col>
         <Col className={styles.right} span={4}>
           <Button type="primary" onClick={() => navigate(`/question/edit/${id}`)}>
